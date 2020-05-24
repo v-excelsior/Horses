e09a104e0d8e7387787db0acefb47936fb398233
My Laptop <vakyla98@ukr.net>
2020-05-24 21:46:34 +0300

    add scss,gulp,modules directory for style, minifyer and map fir styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create()
+    browserSync = require('browser-sync').create(),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
+rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-    return gulp.src('./src/sass/*.scss')
+    return gulp.src('./css/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/style.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watchAll', function () {
@@ -15,8 +28,9 @@ gulp.task('watchAll', function () {
             baseDir: "./",
         }
     });
-    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./css/scss/**/*.scss', gulp.series('sass', 'minify-css')).on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
+
 });
 
 gulp.task("default", gulp.series(gulp.parallel("watchAll")));
